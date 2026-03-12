9a642c3f34eea2a2b242d94d24c1245c39cbd80d
mmaarrttiinneezz07 <martinez8cabrera2007@gmail.com>
2026-03-12 12:17:14 +0100

    card.css creado para el estilo

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,39 +1,16 @@
 import type { InterfazSostenible } from "../data/Ods";
+import "./Card.css";
 
 export const Card = ({ itemOds }: { itemOds: InterfazSostenible }) => {
   return (
-    <div
-      style={{
-        background: itemOds.color,
-        borderRadius: "16px",
-        padding: "20px",
-        border: "1px solid #e5e7eb",
-        boxShadow: "0 10px 20px rgba(0,0,0,0.08)",
-        fontFamily: "system-ui, sans-serif",
-        textAlign: "center",
-      }}
-    >
-      <h3 style={{ color: "#1e293b", marginBottom: "10px" }}>
-        ODS #{itemOds.id}
-      </h3>
+    <div className="card" style={{ background: itemOds.color }}>
+      <h3 className="card-title">ODS #{itemOds.id}</h3>
 
-      <p style={{ color: "#334155", margin: "5px 0" }}>
+      <p className="card-text">
         <strong>ODS favorito:</strong> {itemOds.odsFav}
       </p>
 
-      <p
-        style={{
-          marginTop: "10px",
-          background: "#3b82f6",
-          color: "white",
-          padding: "6px 12px",
-          borderRadius: "8px",
-          display: "inline-block",
-          fontSize: "14px",
-        }}
-      >
-        {itemOds.estado}
-      </p>
+      <p className="card-status">{itemOds.estado}</p>
     </div>
   );
 };
